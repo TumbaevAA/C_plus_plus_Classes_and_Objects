09235b50df2634f975331a650636c953a2162d6a
TumbaevAA <theonetommygun1@gmail.com>
2022-01-11 23:02:49 +0700

    two-dimensional array

--- prog_lab3.cpp
+++ prog_lab3.cpp
@@ -10,6 +10,17 @@ using namespace std;
 int Point::count = 0;
 int main()
 {
+    Point twoDimensionalArray[2][3];
+
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 3; j++) {
+            cout << "\nEnter coordinates of the " << j << " point in the " << i << " row" << endl;
+
+            twoDimensionalArray[i][j].input();
+            twoDimensionalArray[i][j].output();
+        }
+
+
     Point pointArray[3];
 
     for (int i = 0; i < 3; i++) {
