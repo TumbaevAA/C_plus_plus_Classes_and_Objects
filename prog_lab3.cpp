e4afd6b88cb5decee5842ec6de4c85a0679f1c4a
TumbaevAA <theonetommygun1@gmail.com>
2022-01-11 22:54:09 +0700

    one-dimensional array

--- prog_lab3.cpp
+++ prog_lab3.cpp
@@ -10,61 +10,14 @@ using namespace std;
 int Point::count = 0;
 int main()
 {
-    //Тест Circle
-    try {
-        Point p(0, 0);
-        Circle c(p, -1);
-    }
-    catch (const char* e) {
-        cout << e << endl;
-    }
+    Point pointArray[3];
 
-    Circle c1;
-    c1.input();
-
-
-    //Тест Square 
-    try {
-        Point pForSquare(0, 0);
-        Square s(pForSquare, pForSquare, pForSquare, pForSquare);
+    for (int i = 0; i < 3; i++) {
+        cout << "\nEnter coordinates of the " << i << " point" << endl;
+        pointArray[i].input();
+        pointArray[i].output();
     }
-    catch (const char* e) {
-        cout << e << endl;
-    }
-    Square s1;
-    s1.input();
-
-    //Тест Triangle
-    Point pForTriangle(0, 0), pForTriangle1(1, 0), pForTriangle2(0, 1);
-    try {
-        Triangle t(pForTriangle, pForTriangle, pForTriangle);
-    }
-    catch (const char* e) {
-        cout << e << endl;
-    }
-    Triangle t1(pForTriangle, pForTriangle1, pForTriangle2);
-    try {
-        cout << t1.lineLength(4);
-    }
-    catch (const char* e) {
-        cout << e << endl;
-    }
-    t1.input();
-
-
-    //Тест Line
-    Line l;
-    l.input();
-
-    //Тест Point
-    Point p;
-    try {
-        p.input();
-    }
-    catch (const char* e){
-        cout << e << endl;
-    }
-
-
+    
+    cout << endl;
     system("pause");
 }
